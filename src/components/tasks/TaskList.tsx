c6edd3b2de2c13fd7215fb307de5951dd556dd63
jp-mariano <juanpaolo.mariano28@gmail.com>
2025-08-27 14:25:15 +0800

    feat: Initial Task page

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import { 
@@ -136,10 +136,6 @@ export function TaskList({
 
   return (
     <Card>
-      {/* <CardHeader className="pb-2">
-        <CardTitle className="text-base">Tasks</CardTitle>
-      </CardHeader> */}
-      
       <CardContent className="pt-0">
         <div className="space-y-3">
           {priorityGroups.map((group) => {
